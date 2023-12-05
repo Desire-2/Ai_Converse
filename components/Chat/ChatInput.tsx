8fe6fac6f018a07b191d26c766b948c7430e2a4f
Desire-2 <bikorimanadesire5@gmail.com>
2023-12-05 20:30:16 +0200

    Ai Converse

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -386,11 +386,11 @@ export const ChatInput = ({
           rel="noreferrer"
           className="underline"
         >
-          ChatBot UI
+          Ai Converse
         </a>
         .{' '}
         {t(
-          "Chatbot UI is an advanced chatbot kit for OpenAI's chat models aiming to mimic ChatGPT's interface and functionality.",
+          "Ai Converse is an advanced chatbot kit Developed by Desire BIKORIMANA",
         )}
       </div>
     </div>
