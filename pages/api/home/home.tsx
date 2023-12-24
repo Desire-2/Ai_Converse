09bf46efc2dd1f35e99b137ee78ee6462c585ea4
Desire-2 <bikorimanadesire5@gmail.com>
2023-12-24 09:08:05 +0200

    Ai converse

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -361,7 +361,7 @@ const Home = ({
     >
       <Head>
         <title>Ai Converse</title>
-        <meta name="google-adsense-account" content="ca-pub-2776294322568123"></meta>
+        <meta name="google-adsense-account" content="ca-pub-2776294322568123"/>
         <meta name="description" content="The Allien Tv Team." />
         <meta
           name="viewport"
